dede8cb8f47c7d5f0eccd6aa8fb79a8c0302c7f4
ADSZ42 <nnancrs1@gmail.com>
2024-11-25 14:00:50 -0800

    reset fix

--- scripts/app.js
+++ scripts/app.js
@@ -4,13 +4,12 @@ document.getElementById('resetColorBtn').addEventListener('click', resetBoxColor
 function changeBoxColor() {
     const boxNumber = document.getElementById('boxNumber').value.trim();
     const color = document.getElementById('changeColor').value.trim().toLowerCase();
-    const allowedColors = ['red', 'blue', 'purple', 'brown', 'yellow'];
     const box = document.getElementById(`box${boxNumber}`);
     box.style.backgroundColor = color;
 }
 
 function resetBoxColors() {
-    const boxes = document.querySelectorAll('.box');
+    const boxes = document.querySelectorAll('.boxSize');
     boxes.forEach(box => {
         box.style.backgroundColor = '';
     });
